f0bd094e4a1590e9d5ccead739d61c7fbf894554
Morteza Hazrati <morris.hazrati@gmail.com>
2024-11-16 14:27:44 +1100

    Handled loading and error states in the client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -11,6 +11,12 @@ function App() {
 		"http://localhost:4000/balanceReport"
 	)
 
+	// TODO: Use a better loading component
+	if (!data) return <>Loading Data ...</>
+
+	// TODO: Use a better error handling component
+	if (data.Status !== "OK") return <>An error has occurred.</>
+
 	return (
 		<Box px={10}>
 			<BalanceReportTable reports={data.Reports} />
