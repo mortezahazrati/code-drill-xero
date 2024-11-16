1f53f92cf77e6b6bfc484273f3cf40a89cc230f0
Morteza Hazrati <morris.hazrati@gmail.com>
2024-11-16 15:37:32 +1100

    Added the client Dockerfile

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,5 +3,13 @@ import react from "@vitejs/plugin-react"
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react()]
+	plugins: [react()],
+	server: {
+		host: true,
+		port: 8000
+	},
+	preview: {
+		host: true,
+		port: 8000
+	}
 })
